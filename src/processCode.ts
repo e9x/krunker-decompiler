d631d43c1d133d40d6bb02f4b470ff93b3a23cf6
David Reed <reedswork@proton.me>
2023-07-13 02:05:51 +0200

    deobfuscate by default

--- src/processCode.ts
+++ src/processCode.ts
@@ -5,24 +5,15 @@ import { webcrack } from "webcrack";
 
 const programArgv = [...process.argv.slice(2)]; // clone the array to modify it
 
-const deobfuscateI = programArgv.indexOf("--deobfuscate");
-const deobfuscate = deobfuscateI !== -1;
-if (deobfuscate) programArgv.splice(deobfuscateI, 1);
-
 const [file, output] = programArgv;
 
 if (!file) {
-  console.log(`${process.argv
-    .slice(0, 2)
-    .join(" ")} [<file>|<code>] [<output>] [--deobfuscate]
+  console.log(`${process.argv.slice(0, 2).join(" ")} [<file>|<code>] [<output>]
 
 Arguments:
     <file>           Path to a file containing the code
     <code>           The actual code (if not using a file)
-    <output>         Location to output the code (default: stdout)
-
-Options:
-    --deobfuscate    Flag indicating whether the code should be deobfuscated using webcrack`);
+    <output>         Location to output the code (default: stdout)`);
   process.exit(0);
 }
 
@@ -36,14 +27,12 @@ try {
   code = file;
 }
 
-if (deobfuscate) {
-  const { log } = console;
-  console.log = () => {
-    // noop
-  };
-  ({ code } = await webcrack(code));
-  console.log = log;
-}
+const { log } = console;
+console.log = () => {
+  // noop
+};
+({ code } = await webcrack(code));
+console.log = log;
 
 const parsed = processCode(code);
 
